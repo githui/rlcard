9e4d1b71f75a1a9c884bb0fa0ae6aa4ef5b1ec88
hui <sunhui_1998@yahoo.com>
2023-03-06 11:25:28 -0500

    updated judger

--- main.py
+++ main.py
@@ -14,11 +14,10 @@ import numpy as np
 import rlcard
 from rlcard.agents.dmc_agent import DMCTrainer
 # from rlcard.games.findfriends import FindfriendsPlayer as Player
-from rlcard.games.findfriends.player import FindfriendsPlayer as Player
-
+from rlcard.games.findfriends.player import FindfriendsPlayer as Playe
+from rlcard.agents.dqn_agent import DQNAgent
 np_random = np.random.RandomState()
 env=rlcard.make('findfriends')
-
-
-env.set_agents(players)
+agent=DQNAgent()
+env.set_agents(agent)
 env.run()
